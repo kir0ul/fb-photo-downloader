16e131a5ad8c4159ff7bc3f2ac08824bf035a0f9
Andrea Pierré <kir0ul@users.noreply.github.com>
2019-08-14 00:19:46 +0200

    Add working chromedriver + French dateparser

--- get-tagged-photos.py
+++ get-tagged-photos.py
@@ -5,7 +5,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common import exceptions
-from dateutil.parser import parse
+# from dateutil.parser import parse
+from dateparser import parse
 from datetime import datetime
 from datetime import timedelta
 
